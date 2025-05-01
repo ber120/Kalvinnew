31bf844b3f7ebbec66e9177f5f5d3eafdfb4942f
kalvin262 <royalsup87@gmail.com>
2025-05-02 01:26:37 +0530

    Add files via upload

---
+++ vars.py
@@ -0,0 +1,9 @@
+# Don't Remove Credit Tg - WOLVES 🐺
+# Ask Doubt on telegram WOLVES 🐺
+
+from os import environ
+
+API_ID = int(environ.get("API_ID", "20369373"))
+API_HASH = environ.get("API_HASH", "0d53cc7f978163fed3263be5cbb20ab0")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
+AUTH_USERS = "2607963795"
